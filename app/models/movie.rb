a5805c39e7fa0facbaa8a32a275233d87e8664ed
Shuxin Huang <sh4409@columbia.edu>
2023-10-04 21:10:50 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
-class Movie
+class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ['G','PG','PG-13','R']
+  end
+
   def self.with_ratings(ratings_list)
   # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
   #  movies with those ratings
@@ -6,7 +10,6 @@ class Movie
   if ratings_list.present?
     Movie.where(rating: ratings_list)
     else
-      # If ratings_list is nil or empty, retrieve ALL movies
       all
     end
   end
