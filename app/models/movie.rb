0efa5c468aff0ee4e41041094d2ff1a186436747
Shuxin Huang <sh4409@columbia.edu>
2023-10-04 21:34:12 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,7 +8,7 @@ class Movie < ActiveRecord::Base
   # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
   #  movies with those ratings
   # if ratings_list is nil, retrieve ALL movies
-  if ratings_list.present?
+    if ratings_list.present?
     Movie.where(rating: ratings_list.map(&:upcase))
     else
       Movie.all
