477c2558f6bdac4f6e150231716f36826c884a03
Shuxin Huang <sh4409@columbia.edu>
2023-10-05 03:25:58 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,33 +8,32 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
+    @ratings_to_show = @all_ratings
   
     # Check if ratings are in params or session
     if params.has_key?(:ratings)
       @ratings_to_show = params[:ratings].keys
-      session[:ratings] = @ratings_to_show # Store in session
+      session[:ratings] = @ratings_to_show
     elsif session.key?(:ratings)
-      # Redirect to RESTful route with the ratings from session
       redirect_to movies_path(ratings: Hash[session[:ratings].map { |r| [r, '1'] }])
       return # Terminate the action
-    else
-      @ratings_to_show = @all_ratings
     end
   
     # Check if sort_by is in params or session
-    if params.has_key?(:sort_by)
+    if params.has_key?(:sort_by) && ['title', 'release_date'].include?(params[:sort_by])
       @movies = Movie.with_ratings(@ratings_to_show).order(params[:sort_by])
-      @title_header = 'hilite bg-warning' if params[:sort_by] == 'title'
-      @release_date_header = 'hilite bg-warning' if params[:sort_by] == 'release_date'
-      session[:sort_by] = params[:sort_by] # Store in session
-    elsif session.key?(:sort_by)
-      # Redirect to RESTful route with the sort_by from session
-      redirect_to movies_path(sort_by: session[:sort_by], ratings: Hash[session[:ratings].map { |r| [r, '1'] }])
-      return # Terminate the action
+      session[:sort_by] = params[:sort_by]
+    elsif session.key?(:sort_by) && ['title', 'release_date'].include?(session[:sort_by])
+      @movies = Movie.with_ratings(@ratings_to_show).order(session[:sort_by])
     else
       @movies = Movie.with_ratings(@ratings_to_show)
     end
+  
+    # Set the sorting headers
+    @title_header = 'hilite bg-warning' if session[:sort_by] == 'title'
+    @release_date_header = 'hilite bg-warning' if session[:sort_by] == 'release_date'
   end
+  
   
   
 
